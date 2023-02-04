fe452d3d0415abcb2db9f7d4684419746c8bb37b
MatiasxVarela <saxtebg@gmail.com>
2023-02-04 02:38:30 -0300

    Acces controll

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pi-pokemon-main-sooty.vercel.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pi-pokemon-main-6nvbmh1t0-matiasxvarela.vercel.app/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
