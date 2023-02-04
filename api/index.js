6f2854468f0393f205c03a79ab1a951f3ed3c94e
MatiasxVarela <saxtebg@gmail.com>
2023-02-04 01:34:14 -0300

    Fix listen bug

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@
  ➖🟥🟥➖🟥🟥 */
  
 const {
-  DB_PORT
+  PORT
 } = process.env
 
 const server = require('./src/app.js');
@@ -16,7 +16,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(DB_PORT, () => {
-    console.log(`%s listening at ${DB_PORT}`); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
