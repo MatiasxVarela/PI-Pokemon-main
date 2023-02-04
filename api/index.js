5270f19b467cade03d66deec63874900a60b2686
MatiasxVarela <saxtebg@gmail.com>
2023-02-04 01:32:15 -0300

    Change server listen port

--- api/index.js
+++ api/index.js
@@ -7,12 +7,16 @@
  ➖🟥🟥➖🟥🟥
  ➖🟥🟥➖🟥🟥 */
  
+const {
+  DB_PORT
+} = process.env
+
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(DB_PORT, () => {
+    console.log(`%s listening at ${DB_PORT}`); // eslint-disable-line no-console
   });
 });
