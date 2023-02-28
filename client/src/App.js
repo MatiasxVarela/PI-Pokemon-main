8a12a0a69229ee29fdc4051d1d1a879d2ab5c498
MatiasxVarela <saxtebg@gmail.com>
2023-02-28 20:30:22 -0300

    Flydeploy

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import styled from 'styled-components';
 import bgImage from "./img/Bg.png"
 import Form from './components/Form/Form';
 
-axios.defaults.baseURL = 'https://pokemonvrl-production.up.railway.app/'
+axios.defaults.baseURL = 'https://mxvpokemonpi.fly.dev/'
 /* axios.defaults.baseURL = 'http://localhost:3001/' */
 
 const AppDiv = styled.div`
