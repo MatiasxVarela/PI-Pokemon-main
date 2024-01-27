9e1225b84ba3c4bf5bd0327a5be4d5c166d0cf22
MatiasxVarela <matiasxvarela@gmail.com>
2024-01-26 21:56:01 -0300

    Fix info and error cards in mobile

--- client/src/components/PokemonsInfo/ErrorCard.jsx
+++ client/src/components/PokemonsInfo/ErrorCard.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import togepi from "../../img/Togepi.png"
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+import HomeButton from "./HomeButton";
 
 const StyledImg = styled.img`
     width: 300px;
     height: 300px;
+    @media (max-width: 600px) {
+        width: 340px;
+        height: 340px;
+        margin-bottom: 20px;
+    }
 `;
 
 const StyledH2 = styled.h2`
@@ -14,11 +21,26 @@ const StyledH2 = styled.h2`
 `;
 
 export default function ErrorCard(props) {
+    const [windowWidth, setWindowWidth] = useState(window.screen.width);
 
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [setWindowWidth]);
     return (
         <>
-        <StyledH2>{`Pokemon ${props.name} does not exist.`}</StyledH2>
-        <StyledImg src={togepi} alt="" />
+            <StyledH2>{`Pokemon ${props.name} does not exist.`}</StyledH2>
+            <StyledImg src={togepi} alt="" />
+            {windowWidth <= 600
+            && <HomeButton></HomeButton>
+            }
         </>
     );
  }
